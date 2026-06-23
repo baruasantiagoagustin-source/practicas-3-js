c07d31b7aef7ee4df69c4f2cad7be47791e4f976
Santiago Barua <santiago.barua@gm2dev.com>
2026-06-23 12:34:55 -0300

    Basestructure/Exercises

---
+++ practica1.js
@@ -0,0 +1,22 @@
+typeof "Rosario" //string
+typeof 42 //number
+typeof 3.14 //number
+typeof true //boolean
+typeof false //boolean
+typeof undefined //undefined
+typeof null //object
+
+const ciudad    = "Mendoza";
+const habitantes = 115000;
+const esCap     = false;
+const apodo     = null;
+let fundacion;           // sin asignar
+
+console.log(typeof ciudad); //string
+console.log(typeof habitantes); //number
+console.log(typeof esCap); //boolean
+console.log(typeof apodo); //object
+console.log(typeof fundacion); //undefined
+
+const nada = null;
+console.log(typeof nada); // object
