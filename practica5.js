a57373fcb333f4ca768ffd3e8e76c8878c4cde71
Santiago Barua <santiago.barua@gm2dev.com>
2026-06-23 12:12:52 -0300

    Basestructure/Exercises

---
+++ practica5.js
@@ -0,0 +1,9 @@
+const nombre = 'Alexis Mac Allister' 
+let edad = 27 
+const camiseta = 10 
+let goles = 65 
+let asistencias = 41 
+let GyA = 106 
+const equipo = 'liverpool'
+
+console.log(`El jugador ${nombre} con una edad de ${edad} tiene en su cuenta ya ${GyA} G/A. Juega en ${equipo} como el numero ${camiseta}`)
