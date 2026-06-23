9e701ac009fe9ca85f36e1daabed8fab8a440b03
Santiago Barua <santiago.barua@gm2dev.com>
2026-06-23 12:51:35 -0300

    Basestructure/Exercises

---
+++ practica3.js
@@ -0,0 +1,13 @@
+const nombre = "Lautaro";
+const edad   = 17;
+const ciudad = "Rosario";
+const equipo = "Newells";
+const texto = `Nombre: ${nombre}
+Edad: ${edad}
+Ciudad: ${ciudad}`
+
+console.log(`Hola, me llamo ${nombre} y tengo ${edad} años.`)
+console.log(`Soy de ${ciudad}.`)
+console.log(`El doble de mi edad es ${edad * 2}.`)
+console.log(`Soy ${nombre} de ${ciudad} y sigo a ${equipo}.`)
+console.log(texto)
