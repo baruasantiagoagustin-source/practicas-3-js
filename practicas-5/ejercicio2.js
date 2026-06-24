50ea53ecc25ead58556c0f26fc617043bf025006
Santiago Barua <santiago.barua@gm2dev.com>
2026-06-24 12:48:40 -0300

    Baseestructure/Exercises

---
+++ practicas-5/ejercicio2.js
@@ -0,0 +1,32 @@
+//parte A
+
+// function triple(n) {
+//   console.log(n * 3);
+// }
+
+// const a = triple(4);
+// console.log(a); //no tiene definido a, devuelve undefined
+// console.log(triple(4) + 1); //hace eso porque imprime primero el resultado de triple(), pero devuelve undefined y luego hace la cuenta entonce nos da NaN
+
+//parte B
+
+// function precioConIva(precio) {
+//   return precio * 1.21; //usamos return para que devuelva un numero y luego imprima el total
+// }
+
+// const total = precioConIva(100);
+// console.log(`El total es ${total}`);  
+
+//PARTE C
+
+function calcularEdad(anioNacimiento) {
+    return `tu edad es ${resultado = 2026 - anioNacimiento}` 
+}
+
+console.log(calcularEdad(1990))
+
+function mostrarBienvenida() {
+    console.log('Hola, bienvenido a la consola')
+}
+
+mostrarBienvenida()
