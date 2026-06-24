17be450d018fc36e2c9391cd1baa87f7d689c353
Santiago Barua <santiago.barua@gm2dev.com>
2026-06-24 12:59:36 -0300

    Baseestructure/Exercises

---
+++ practicas-5/ejercicio3.js
@@ -0,0 +1,20 @@
+//parte a
+
+// function presentar(nombre, ciudad) {
+//   return `${nombre} es de ${ciudad}`;
+// }
+
+// console.log(presentar("Ana", "Rosario")); //logre predecirlo, dio ana es de rosario
+// console.log(presentar("Ana")); // no logre predecirlo, no crei que cuando un dato no es puesto daria undefined 
+// console.log(presentar()); // lo mismo de antes, dio undefined en ambas
+
+function saludar(nombre, idioma) {
+    if (idioma == 'en') {
+        console.log(`Hello, ${nombre}`)
+    }
+    console.log(`Hola, ${nombre}`) // lo dejo por fuera para que sea el por defecto
+}
+
+saludar("Ana");          // "Hola, Ana"
+saludar("Ann", "en");    // "Hello, Ann"
+saludar("Ana", "es");    // "Hola, Ana"
