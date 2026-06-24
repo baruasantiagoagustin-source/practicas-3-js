0c53b4b9e6f8f26ccb1ef80b5a00b1a385afea1b
Santiago Barua <santiago.barua@gm2dev.com>
2026-06-24 12:20:43 -0300

    Baseestructure/Exercises

---
+++ practicas-5/ejercicio1.js
@@ -0,0 +1,28 @@
+//PARTE A
+
+// function areaRectangulo(base, altura) {
+//     let resultado = base * altura;
+//     return resultado;
+// }
+
+// console.log(areaRectangulo(4, 5)); //20
+// console.log(areaRectangulo(3, 3)); //9
+
+//PARTE B
+// function esMayorDeEdad(edad) {
+//     if (edad < 18) {
+//         return false
+//     } else {
+//         return true
+//     }
+// }
+
+
+// console.log(esMayorDeEdad(20));   // true
+// console.log(esMayorDeEdad(15));   // false
+//parte C
+function nombreCompleto(nombre, apellido) {
+    return `Nombre completo: ${nombre} ${apellido}`
+}
+
+console.log(nombreCompleto("Ana", "Pérez"));   // "Ana Pérez"
