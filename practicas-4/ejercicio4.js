ecd25cc3f615192d035e0fdd27f5461a908e8291
Santiago Barua <santiago.barua@gm2dev.com>
2026-06-24 11:09:55 -0300

    Baseestructure/Exercises

--- practicas-4/ejercicio4.js
+++ practicas-4/ejercicio4.js
@@ -1,12 +1,16 @@
-const numeroSecreto = 7;
-const intentos = [3, 9, 1, 7, 5]; // el jugador "intenta" estos números en orden
+const numeroSecreto = 42;
+const intentos = [3, 9, 1, 7, 5, 4, 10, 15, 17]; // el jugador "intenta" estos números en orden
 
 let i = 1;
 for (const intento of intentos) {
     if (intento === numeroSecreto) {
         console.log(`intento ${i} ${intento}: adivinaste en ${i} intentos!`)
         break
+    } else if (i === 8) {
+        console.log(`intento ${i} ${intento}: Lo siento, no adivinaste, el numero era ${numeroSecreto}`)
+        break
+    } else {
+        console.log(`intento ${i} ${intento}`);
+        i++;
     }
-    console.log(`intento ${i} ${intento}`);
-    i++;
 }
