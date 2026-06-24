50c786bce700bed9e2de878a82db2007ec5a03f3
Santiago Barua <santiago.barua@gm2dev.com>
2026-06-24 10:07:00 -0300

    Basestructure/Exercises

---
+++ practicas-4/ejercicio1.js
@@ -0,0 +1,13 @@
+//parte A
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i)
+// }
+
+//parte B
+
+
+for (let i = 1; i <= 10; i++) {
+    if(i % 2 === 0) {console.log(i)} //preferi usar la 2da opcion, me parece mejor practicamente para poder escalar el codigo a futuro.
+}
+
