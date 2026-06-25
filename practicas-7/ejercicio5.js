ee608b1bba829b8fc668d09ac1ce25246c99ff39
Santiago Barua <santiago.barua@gm2dev.com>
2026-06-25 12:56:29 -0300

    Baseestructure/Exercises

---
+++ practicas-7/ejercicio5.js
@@ -0,0 +1,8 @@
+const jugadores = [
+  { nombre: "Messi",   goles: 8,  partidos: 7 },
+  { nombre: "Mbappé",  goles: 5,  partidos: 6 },
+  { nombre: "Haaland", goles: 12, partidos: 6 },
+  { nombre: "Kane",    goles: 0,  partidos: 5 },
+  { nombre: "Neymar",  goles: 3,  partidos: 7 },
+];
+
